249cd1a3604df07dbfdfc39866840f2d5416c42e
kamilawad1 <kamilawad15@gmail.com>
2024-05-01 00:44:35 +0300

    backend - importing config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/biketrackdb'), UsersModule],
+  imports: [ConfigModule.forRoot(),
+  MongooseModule.forRoot('mongodb://localhost/biketrackdb'), UsersModule],
   controllers: [],
   providers: [],
 })
