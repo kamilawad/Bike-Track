de580206efe27dd484e15d6d5966f4e0e33300e2
kamilawad1 <kamilawad15@gmail.com>
2024-04-30 11:21:05 +0300

    update user endpoint

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Post, Controller, UsePipes, ValidationPipe, Get, Param, HttpException } from "@nestjs/common";
+import { Body, Post, Controller, UsePipes, ValidationPipe, Get, Param, HttpException, Patch, NotFoundException } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDTO } from "./dto/CreateUser.dto";
 import mongoose from "mongoose";
+import { UpdateUserDto } from "./dto/UpdateUser.dto";
 
 
 @Controller('users')
@@ -26,5 +27,15 @@ export class UsersController {
         const findUser = await this.usersService.getUserById(id);
         if (!findUser) throw new HttpException('User not found', 404);
         return findUser;
+    }
+
+    @Patch(':id')
+    @UsePipes(new ValidationPipe())
+    async updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        const updatedUser = await this.usersService.updateUser(id, updateUserDto);
+        if (!updatedUser) {
+            throw new NotFoundException('User not found');
+        }
+        return updatedUser;
     }
 }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "src/schemas/User.schema";
@@ -22,7 +22,18 @@ export class UsersService {
         return this.userModel.findById(id);
     }
 
-    updateUser(id: string, updateUserDto: UpdateUserDto) {
-        return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true });
+    async updateUser(id: string, updateUserDto: UpdateUserDto) {
+        const user = await this.userModel.findById(id);
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        if (updateUserDto.fullName) {
+            user.fullName = updateUserDto.fullName;
+        }
+        if (updateUserDto.password) {
+            user.password = updateUserDto.password;
+        }
+        return user.save();
+        //return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true });
     }
 }
