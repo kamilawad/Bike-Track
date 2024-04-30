1eecaaf30562bb94bb48fb6f188db84171347a10
kamilawad1 <kamilawad15@gmail.com>
2024-04-30 09:15:02 +0300

    create and save initial user to mongodb

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -2,8 +2,14 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "src/schemas/User.schema";
+import { CreateUserDTO } from "./dto/CreateUser.dto";
 
 @Injectable()
 export class UsersService {
     constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+    createUser(createUserDto: CreateUserDTO) {
+        const newUser = new this.userModel(createUserDto);
+        return newUser.save();
+    }
 }
