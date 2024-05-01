6873262eeb071dbd2de4ef5c406a83d1406bf21f
kamilawad1 <kamilawad15@gmail.com>
2024-05-02 01:30:44 +0300

    backend - destructure signup dto

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "src/schemas/user.schema";
+import { SignUpDto } from "./dto/signup.dto";
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectModel(User.name)
-        private userModel: Model<User>) {}
+        private userModel: Model<User>
+    ) {}
+
+    async signUp(signUpDto: SignUpDto) {
+        const { fullName, email, password} = signUpDto;
+    }
 }
