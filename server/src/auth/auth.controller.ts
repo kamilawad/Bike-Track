bbb394ef94bd99177a3debaf1238a11e47f39fb6
kamilawad1 <kamilawad15@gmail.com>
2024-05-01 19:16:46 +0300

    backend - add auth controller constructor

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
 import { Controller } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User } from "src/schemas/user.schema";
 
 
 @Controller('auth')
 export class AuthController {
-    
+    constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 }
