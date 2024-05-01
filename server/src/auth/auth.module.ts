85ade1779c01ffa98d25cbca24cec13e401c1249
kamilawad1 <kamilawad15@gmail.com>
2024-05-01 18:37:11 +0300

    backend - setup auth module file

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,7 +1,11 @@
-import { Controller } from "@nestjs/common";
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
 
-@Controller('auth')
-export class AuthController {
-    
-}
+@Module({
+    imports: [],
+    controllers: [AuthController],
+    providers: [AuthService]
+})
+export class AuthModule {}
