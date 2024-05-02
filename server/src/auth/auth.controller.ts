eb41aa24dd92dabe1169af200fc05a96db433386
kamilawad1 <kamilawad15@gmail.com>
2024-05-02 09:27:41 +0300

    backend - add login endpoint

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { SignUpDto } from "./dto/signup.dto";
+import { LogInDto } from "./dto/login.dto";
 
 @Controller('auth')
 export class AuthController {
     constructor( private authService: AuthService) {}
 
     @Post('/signup')
-    signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
-        return this.authService.signUp(signUpDto);
+    signUp(@Body() signupDto: SignUpDto): Promise<{ token: string }> {
+        return this.authService.signUp(signupDto);
+    }
+
+    @Post('/login')
+    login(@Body() loginDto: LogInDto): Promise<{ token: string }> {
+        return this.authService.login(loginDto);
     }
 }
