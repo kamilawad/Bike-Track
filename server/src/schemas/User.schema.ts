cec97ef4850877aca80b9cc32af4d10a2deb69d3
kamilawad1 <kamilawad15@gmail.com>
2024-05-02 09:55:11 +0300

    backend - update user role schema

--- server/src/schemas/User.schema.ts
+++ server/src/schemas/User.schema.ts
@@ -1,5 +1,11 @@
 import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
 
+export enum UserRole {
+    USER = 'user',
+    ORGANIZER = 'organizer',
+    ADMIN = 'admin'
+}
+
 @Schema({
     timestamps: true
 })
@@ -13,8 +19,8 @@ export class User {
     @Prop({ required: true })
     password: string;
 
-    @Prop({ required: true, enum: ['user', 'organizer', 'admin'], default: 'user' })
-role: string;
+    @Prop({ required: true, enum: UserRole, default: 'user' })
+role: UserRole;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
