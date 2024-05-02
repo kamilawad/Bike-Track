a66740425af019206dffb004d5c8cf9c80c2589f
kamilawad1 <kamilawad15@gmail.com>
2024-05-02 09:41:13 +0300

    backend - add user role to user schema

--- server/src/schemas/User.schema.ts
+++ server/src/schemas/User.schema.ts
@@ -12,6 +12,9 @@ export class User {
 
     @Prop({ required: true })
     password: string;
+
+    @Prop({ required: true, enum: ['user', 'organizer', 'admin'], default: 'user' })
+role: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
