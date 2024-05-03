b4afe959f847e084abc8b5bb55da2ae74386217f
kamilawad1 <kamilawad15@gmail.com>
2024-05-03 15:24:31 +0300

    setup jwt strategy file

---
+++ server/src/auth/jwt.strategy.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    
+}
