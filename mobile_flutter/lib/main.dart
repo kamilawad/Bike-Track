641f9480e43fe4e3e00ef73e94d510db3d5f28da
kamilawad1 <kamilawad15@gmail.com>
2024-05-04 02:06:54 +0300

    frontend - setup routes

--- mobile_flutter/lib/main.dart
+++ mobile_flutter/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_flutter/screens/auth/login_screen.dart';
+import 'package:mobile_flutter/screens/auth/signup_screen.dart';
+import 'package:mobile_flutter/screens/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +18,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      routes: {
+        "/welcome": (context) => WelcomeScreen(),
+        "/signup": (context) => SignupScreen(),
+        "/login": (context) => LoginScreen(),
+      }
     );
   }
 }
