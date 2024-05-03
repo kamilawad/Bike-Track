47677425b0a57eabd0971878b2972c258e729a2d
kamilawad1 <kamilawad15@gmail.com>
2024-05-03 17:04:43 +0300

    backend - import auth module into user module

--- server/src/users/user.module.ts
+++ server/src/users/user.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/user.schema';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
     imports: [
+        AuthModule,
         MongooseModule.forFeature([
             {
                 name:User.name,
