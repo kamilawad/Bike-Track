3b51eed9e3162e4bc61c75683c5b2f14b5177e65
kamilawad1 <kamilawad15@gmail.com>
2024-05-03 15:44:33 +0300

    backend - inject user model intp jwt strategy

--- server/src/auth/jwt.strategy.ts
+++ server/src/auth/jwt.strategy.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
 import { PassportStrategy } from "@nestjs/passport";
+import { Model } from "mongoose";
+import { Strategy } from "passport-local";
+import { User } from "src/schemas/user.schema";
 
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    
+    constructor(
+        @InjectModel(User.name) private userModel: Model<User>
+    ) {
+        super();
+    }
 }
