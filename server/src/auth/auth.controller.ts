ea2e819f8bba326b1fa8d092da56d500fde2a637
kamilawad1 <kamilawad15@gmail.com>
2024-05-05 19:05:45 +0300

    backend - fix validation

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, ValidationPipe, UsePipes } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { SignUpDto } from "./dto/signup.dto";
 import { LogInDto } from "./dto/login.dto";
@@ -8,11 +8,13 @@ export class AuthController {
     constructor( private authService: AuthService) {}
 
     @Post('/signup')
+    @UsePipes(new ValidationPipe())
     signUp(@Body() signupDto: SignUpDto): Promise<{ token: string }> {
         return this.authService.signUp(signupDto);
     }
 
     @Post('/login')
+    @UsePipes(new ValidationPipe())
     login(@Body() loginDto: LogInDto): Promise<{ token: string }> {
         return this.authService.login(loginDto);
     }
