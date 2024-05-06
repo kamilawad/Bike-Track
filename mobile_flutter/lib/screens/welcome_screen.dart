943b186c98755779fb8d00c3cca704aa66e47e26
kamilawad1 <kamilawad15@gmail.com>
2024-05-06 23:50:05 +0300

    frontend - style welcome screen

--- mobile_flutter/lib/screens/welcome_screen.dart
+++ mobile_flutter/lib/screens/welcome_screen.dart
@@ -13,6 +13,60 @@ class WelcomeScreen extends StatelessWidget {
             'assets/welcome.png',
             fit: BoxFit.cover,
           ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              color: Colors.black.withOpacity(0.46),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Welcome Kamil!',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFF05206),
+                    ),
+                  ),
+
+                  const SizedBox(height:5),
+                  const Text(
+                    'Welcome to Biketrack community',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: const Color(0xFFF05206),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "Finish",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
