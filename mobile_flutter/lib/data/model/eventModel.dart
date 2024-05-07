e79a75117eff8c51e7b8edb9320b8fd6527f346f
kamilawad1 <kamilawad15@gmail.com>
2024-05-07 21:01:31 +0300

    frontend - create event model

---
+++ mobile_flutter/lib/data/model/eventModel.dart
@@ -0,0 +1,8 @@
+class EventModel {
+  String eventName;
+  String eventDescription;
+  String imagePath;
+  EventModel(
+    {required this.eventName, required this.eventDescription, required this.imagePath}
+  );
+}
