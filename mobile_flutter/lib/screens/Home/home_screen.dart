3fc066f085f00d57f77f8b292e3619e43f9673b6
kamilawad1 <kamilawad15@gmail.com>
2024-05-07 12:47:11 +0300

    frontend - add navigation bar icons

--- mobile_flutter/lib/screens/Home/home_screen.dart
+++ mobile_flutter/lib/screens/Home/home_screen.dart
@@ -9,11 +9,12 @@ class HomeScreen extends StatelessWidget {
       bottomNavigationBar: NavigationBar(
         destinations: const [
           NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.map_outlined), label: "MAps"),
+          NavigationDestination(icon: Icon(Icons.route), label: "Routes"),
+          NavigationDestination(icon: Icon(Icons.event), label: "Events"),
+          NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
         ],
-        ),
+      ),
     );
   }
 }
