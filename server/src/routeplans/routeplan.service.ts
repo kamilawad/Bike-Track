1106203897ec3c1de744a122a6960279e4c0770a
kamilawad1 <kamilawad15@gmail.com>
2024-05-07 11:56:26 +0300

    backend - create route plan service

--- server/src/routeplans/routeplan.service.ts
+++ server/src/routeplans/routeplan.service.ts
@@ -11,5 +11,14 @@ export class RoutePlanService {
     constructor(@InjectModel(RoutePlan.name) private routePlanModel: Model<RoutePlan>,
     @InjectModel(User.name) private userModel: Model<User>) {}
 
-    
+    async createRoutePlan(id: string, createRoutePlanDto: CreateRoutePlanDto) : Promise<RoutePlan> {
+        const newRoutePlan = new this.routePlanModel(createRoutePlanDto);
+        const user = await this.userModel.findById(id);
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        user.savedRoutes.push(newRoutePlan);
+        await user.save();
+        return await newRoutePlan.save();
+    }
 }
