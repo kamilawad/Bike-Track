069cec7f9b18ab5beee870222c0f1115acffd7b5
kamilawad1 <kamilawad15@gmail.com>
2024-05-07 10:48:39 +0300

    backend - add route controller constructor

--- server/src/routeplans/routeplan.controller.ts
+++ server/src/routeplans/routeplan.controller.ts
@@ -1,7 +1,8 @@
 import { Controller } from "@nestjs/common";
+import { RoutePlanService } from "./routeplan.service";
 
 
 @Controller('users')
 export class RoutePlanController {
-    
+    constructor(private readonly routePlanService: RoutePlanService) {}
 }
