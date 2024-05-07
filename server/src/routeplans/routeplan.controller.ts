87312b2c2c6a51c1c61d493b9031087ed87be124
kamilawad1 <kamilawad15@gmail.com>
2024-05-07 10:54:05 +0300

    backend - add route service constructor

--- server/src/routeplans/routeplan.controller.ts
+++ server/src/routeplans/routeplan.controller.ts
@@ -1,8 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { RoutePlanService } from "./routeplan.service";
+import { AuthGuard } from "@nestjs/passport";
+import { CreateRoutePlanDto } from "./dto/create-route.dto";
 
 
 @Controller('users')
 export class RoutePlanController {
     constructor(private readonly routePlanService: RoutePlanService) {}
+
+    
 }
--- server/src/routeplans/routeplan.service.ts
+++ server/src/routeplans/routeplan.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { RoutePlan } from "src/schemas/routeplan.schema";
 
 
 @Injectable()
-export class RoutePlanService {}
+export class RoutePlanService {
+    constructor(@InjectModel(RoutePlan.name) private routePlanModel: Model<RoutePlan>) {}
+
+}
