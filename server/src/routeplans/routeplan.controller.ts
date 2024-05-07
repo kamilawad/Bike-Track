f030ca8f9bff6d8fd2b532bd2e3a169c15911404
kamilawad1 <kamilawad15@gmail.com>
2024-05-07 12:09:04 +0300

    backend - add delete route endpoint

--- server/src/routeplans/routeplan.controller.ts
+++ server/src/routeplans/routeplan.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Param, Post, Request, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Request, UseGuards } from "@nestjs/common";
 import { RoutePlanService } from "./routeplan.service";
 import { AuthGuard } from "@nestjs/passport";
 import { CreateRoutePlanDto } from "./dto/create-route.dto";
+import { UpdateRoutePlanDto } from "./dto/update-route.dto";
 
 
 @Controller('routes')
@@ -18,5 +19,17 @@ export class RoutePlanController {
     @UseGuards(AuthGuard('jwt'))
     async getSavedRoutes(@Request() req) {
         return this.routePlanService.getSavedRoutes(req.user.id);
+    }
+
+    /*@Patch(':id')
+    @UseGuards(AuthGuard('jwt'))
+    async updateRoutePlan(@Request() req, @Param('id') id: string, @Body() updateRoutePlanDto: UpdateRoutePlanDto) {
+        return this.routePlanService.updateRoutePlan(req.user.id, id, updateRoutePlanDto);
+    }*/
+
+    @Delete(':id')
+    @UseGuards(AuthGuard('jwt'))
+    async deleteRoutePlan(@Request() req, @Param('id') id: string) {
+        return this.routePlanService.deleteRoutePlan(req.user.id, id);
     }
 }
