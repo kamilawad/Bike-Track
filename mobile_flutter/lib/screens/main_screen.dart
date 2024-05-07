711e2ba4be9e7cd36a6e88b2bdf35f2c29815ff1
kamilawad1 <kamilawad15@gmail.com>
2024-05-07 18:32:43 +0300

    frontend - add switch between buttons in navigation bar

--- mobile_flutter/lib/screens/main_screen.dart
+++ mobile_flutter/lib/screens/main_screen.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int currentScreen = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentScreen,
+        onTap: (value) {
+          setState(() {
+            currentScreen = value;
+          });
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
