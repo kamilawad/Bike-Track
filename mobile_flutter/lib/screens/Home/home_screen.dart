66769a1e5de5ca047a1be0ac930b0822a1dd5550
kamilawad1 <kamilawad15@gmail.com>
2024-05-07 12:43:42 +0300

    frontend - add home screen navigation bar

--- mobile_flutter/lib/screens/Home/home_screen.dart
+++ mobile_flutter/lib/screens/Home/home_screen.dart
@@ -5,6 +5,15 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+        ],
+        ),
+    );
   }
 }
