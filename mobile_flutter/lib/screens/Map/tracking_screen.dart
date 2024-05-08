467fccac6b7eedf491c8f4c4f4b831d32c66d615
kamilawad1 <kamilawad15@gmail.com>
2024-05-08 18:24:34 +0300

    frontend - style track screen

--- mobile_flutter/lib/screens/Map/tracking_screen.dart
+++ mobile_flutter/lib/screens/Map/tracking_screen.dart
@@ -11,6 +11,15 @@ class TrackScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
         centerTitle: true,
+        leading: IconButton(
+          icon: Transform.scale(
+            scale: 0.9,
+            child: const Icon(Icons.arrow_back_ios_new_rounded, color: Colors.black),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
 
       body: const Center(
@@ -27,7 +36,7 @@ class TrackScreen extends StatelessWidget {
           backgroundColor: const Color(0xFFF05206),
           elevation: 0,
           highlightElevation: 0,
-          child: const Icon(Icons.stop)
+          child: const Icon(Icons.stop, size: 36,)
         ),
       ),
       ),
