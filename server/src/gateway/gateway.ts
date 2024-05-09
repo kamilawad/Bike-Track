96e04aed798f70c7f024dde5d64db61f2c056269
kamilawad1 <kamilawad15@gmail.com>
2024-05-09 18:12:28 +0300

    backend - implements onModuleInit interface

--- server/src/gateway/gateway.ts
+++ server/src/gateway/gateway.ts
@@ -1,10 +1,15 @@
+import { OnModuleInit } from "@nestjs/common";
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server } from 'socket.io'
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnModuleInit{
     @WebSocketServer()
     server: Server;
+
+    onModuleInit() {
+        
+    }
 
     @SubscribeMessage('newMessage')
     onNewMessage(@MessageBody() body: any) {
