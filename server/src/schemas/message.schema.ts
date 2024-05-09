8baadda1ddcda6d20d7856ea95ffdbaae39ea444
kamilawad1 <kamilawad15@gmail.com>
2024-05-09 23:59:05 +0300

    backend - add timestamps to message schema

--- server/src/schemas/message.schema.ts
+++ server/src/schemas/message.schema.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import mongoose from "mongoose";
 import { User } from "./user.schema";
 
-@Schema()
+@Schema({ timestamps: true })
 export class Message extends Document {
     @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "User" })
     sender: User;
