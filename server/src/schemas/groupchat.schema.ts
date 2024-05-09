a4d119c52f5064838aac97d7cb1819cb2752da68
kamilawad1 <kamilawad15@gmail.com>
2024-05-09 23:46:10 +0300

    backend - add group chat schema

--- server/src/schemas/groupchat.schema.ts
+++ server/src/schemas/groupchat.schema.ts
@@ -1,7 +1,12 @@
 import { Prop, Schema } from "@nestjs/mongoose";
 import mongoose, { Document } from "mongoose";
+import { User } from "./user.schema";
 
 @Schema()
 export class Chat extends Document{
-    
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }] })
+    members: User[];
 }
