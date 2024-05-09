616cbd145b60ee465269aa52afea2782d6497674
kamilawad1 <kamilawad15@gmail.com>
2024-05-10 02:06:39 +0300

    backend - register ch

--- server/src/chats/chat.module.ts
+++ server/src/chats/chat.module.ts
@@ -1,4 +1,18 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AuthModule } from "src/auth/auth.module";
+import { Chat, ChatSchema } from "src/schemas/chat.schema";
+import { Message, MessageSchema } from "src/schemas/message.schema";
+import { User, UserSchema } from "src/schemas/user.schema";
 
-@Module({})
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+          { name: Chat.name, schema: ChatSchema },
+          { name: User.name, schema: UserSchema },
+          { name: Message.name, schema: MessageSchema },
+        ]),
+        AuthModule,
+    ],
+})
 export class ChatModule {}
