469c1ba25e8d4488880ece3a9f372bbdcb402ab0
kamilawad1 <kamilawad15@gmail.com>
2024-05-10 02:14:29 +0300

    backend - inject models to chat service

--- server/src/chats/chat.service.ts
+++ server/src/chats/chat.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Chat } from "src/schemas/chat.schema";
+import { Message } from "src/schemas/message.schema";
+import { User } from "src/schemas/user.schema";
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+    constructor(
+        @InjectModel(Chat.name) private chatModel: Model<Chat>,
+        @InjectModel(User.name) private userModel: Model<User>,
+        @InjectModel(Message.name) private messageModel: Model<Message>
+    ) {}
+}
