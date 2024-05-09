07eb94d9753db20ed1aafd65b6d30969b6b8201a
kamilawad1 <kamilawad15@gmail.com>
2024-05-09 23:56:57 +0300

    backend - add fields to message schema

--- server/src/schemas/message.schema.ts
+++ server/src/schemas/message.schema.ts
@@ -1,6 +1,15 @@
-import { Schema } from "@nestjs/mongoose";
+import { Prop, Schema } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+import { User } from "./user.schema";
 
 @Schema()
 export class Message extends Document {
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "User" })
+    sender: User;
   
+    @Prop({ required: true })
+    content: string;
+  
+    @Prop({ required: true })
+    sentAt: Date;
 }
