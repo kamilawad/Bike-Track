2bf8fde1b8631992c6ca403bc5d3f7a11edb30e1
kamilawad1 <kamilawad15@gmail.com>
2024-05-09 18:09:45 +0300

    backend - declare seckot.io server

--- server/src/gateway/gateway.ts
+++ server/src/gateway/gateway.ts
@@ -1,9 +1,13 @@
 import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { Server } from 'socket.io'
 
 @WebSocketGateway()
 export class ChatGateway {
+    server: Server;
+
     @SubscribeMessage('newMessage')
     onNewMessage(@MessageBody() body: any) {
+        //log message body in the console
         console.log(body);
     }
 }
