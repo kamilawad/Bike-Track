50291f6c20cf0b506c14d254572c198823223d64
kamilawad1 <kamilawad15@gmail.com>
2024-05-09 23:43:32 +0300

    backend - update chat schema fields

--- server/src/schemas/chat.schema.ts
+++ server/src/schemas/chat.schema.ts
@@ -4,9 +4,9 @@ import { User } from "./user.schema";
 
 @Schema()
 export class Chat extends Document{
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "User" })
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, })
     sender: User;
 
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "User" })
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, })
     receiver: User;
 }
