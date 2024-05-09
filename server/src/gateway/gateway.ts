965e67621ef4ea2fd080a9c8ffa07946f1cb14e8
kamilawad1 <kamilawad15@gmail.com>
2024-05-09 18:10:47 +0300

    backend - attach web socket server to the server property

--- server/src/gateway/gateway.ts
+++ server/src/gateway/gateway.ts
@@ -1,8 +1,9 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server } from 'socket.io'
 
 @WebSocketGateway()
 export class ChatGateway {
+    @WebSocketServer()
     server: Server;
 
     @SubscribeMessage('newMessage')
