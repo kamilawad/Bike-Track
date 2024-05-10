c6b1b4961d1d8d7ec9814e99a806c4822ec4bfcc
kamilawad1 <kamilawad15@gmail.com>
2024-05-11 01:38:20 +0300

    backend - import models to group chat module

--- server/src/groupchats/group-chat.module.ts
+++ server/src/groupchats/group-chat.module.ts
@@ -1,7 +1,17 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { GroupChat, GroupChatSchema } from "src/schemas/groupchat.schema";
+import { Message, MessageSchema } from "src/schemas/message.schema";
+import { User, UserSchema } from "src/schemas/user.schema";
 
 @Module({
-    imports: [],
+    imports: [
+        MongooseModule.forFeature([
+            { name: GroupChat.name, schema: GroupChatSchema },
+            { name: User.name, schema: UserSchema },
+            { name: Message.name, schema: MessageSchema },
+        ]),
+    ],
     controllers: [],
     providers: [],
 })
