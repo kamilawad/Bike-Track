50936a648e32e2163ad8b2f085f8bd19fd2aa55f
kamilawad1 <kamilawad15@gmail.com>
2024-05-11 01:43:41 +0300

    backend - inject required models

--- server/src/groupchats/group-chat.service.ts
+++ server/src/groupchats/group-chat.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { GroupChat } from "src/schemas/groupchat.schema";
+import { Message } from "src/schemas/message.schema";
+import { User } from "src/schemas/user.schema";
 
 @Injectable()
-export class GroupChatService {}
+export class GroupChatService {
+    constructor(
+        @InjectModel(GroupChat.name) private groupChatModel: Model<GroupChat>,
+        @InjectModel(User.name) private userModel: Model<User>,
+        @InjectModel(Message.name) private messageModel: Model<Message>
+    ) {}
+}
