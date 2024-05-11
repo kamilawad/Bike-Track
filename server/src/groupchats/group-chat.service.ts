964c31b53d9919ca99794f151795db24213d171e
kamilawad1 <kamilawad15@gmail.com>
2024-05-11 11:15:34 +0300

    backend - create groupchat service

--- server/src/groupchats/group-chat.service.ts
+++ server/src/groupchats/group-chat.service.ts
@@ -4,6 +4,7 @@ import { Model } from "mongoose";
 import { GroupChat } from "src/schemas/groupchat.schema";
 import { Message } from "src/schemas/message.schema";
 import { User } from "src/schemas/user.schema";
+import { CreateGroupChatDto } from "./dto/create-group-chat.dto";
 
 @Injectable()
 export class GroupChatService {
@@ -12,4 +13,25 @@ export class GroupChatService {
         @InjectModel(User.name) private userModel: Model<User>,
         @InjectModel(Message.name) private messageModel: Model<Message>
     ) {}
+
+    async createGroupChat(
+        createGroupChatDto: CreateGroupChatDto, adminId: string): Promise<GroupChat> {
+        const { name, memberIds } = createGroupChatDto;
+
+        const members = await this.userModel.find({ _id: { $in: memberIds } });
+        const admin = await this.userModel.findById(adminId);
+
+        if (!admin) {
+            throw new Error("admin not found");
+        }
+
+        const groupChat = new this.groupChatModel({
+            name,
+            members,
+            admins: [admin],
+            messages: [],
+        });
+
+        return groupChat.save();
+    }
 }
