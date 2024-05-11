a202e50932cdbb24e18ad59cce9c40c149dafc5b
kamilawad1 <kamilawad15@gmail.com>
2024-05-11 19:26:05 +0300

    backend - add create group chat endpoint

--- server/src/groupchats/group-chat.controller.ts
+++ server/src/groupchats/group-chat.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, Request, UseGuards } from "@nestjs/common";
 import { GroupChatService } from "./group-chat.service";
 import { AuthGuard } from "@nestjs/passport";
+import { CreateGroupChatDto } from "./dto/create-group-chat.dto";
 
 @Controller()
 @UseGuards(AuthGuard("jwt"))
 export class GroupChatController {
     constructor(private readonly groupChatService: GroupChatService) {}
+
+    @Post()
+    createGroupChat( @Request() req, @Body() createGroupChatDto: CreateGroupChatDto) {
+        return this.groupChatService.createGroupChat(createGroupChatDto, req.user._id);
+    }
 }
