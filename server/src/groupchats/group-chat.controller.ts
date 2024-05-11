18e34c2eafe5902ed377dd171fe47c34d142cfa1
kamilawad1 <kamilawad15@gmail.com>
2024-05-11 19:21:01 +0300

    backend - add controller decorator

--- server/src/groupchats/group-chat.controller.ts
+++ server/src/groupchats/group-chat.controller.ts
@@ -1,5 +1,7 @@
+import { Controller } from "@nestjs/common";
 import { GroupChatService } from "./group-chat.service";
 
+@Controller()
 export class GroupChatController {
     constructor(private readonly groupChatService: GroupChatService) {}
 }
