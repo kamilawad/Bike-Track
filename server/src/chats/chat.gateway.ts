5990869be289e3b69dd8a56b6ac35f8630787c2b
kamilawad1 <kamilawad15@gmail.com>
2024-05-12 15:02:26 +0300

    backend - imject chat service instance

--- server/src/chats/chat.gateway.ts
+++ server/src/chats/chat.gateway.ts
@@ -1,11 +1,13 @@
 import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Server } from 'socket.io';
-import { Socket } from "socket.io-client";
+import { Server, Socket } from 'socket.io';
+import { ChatService } from "./chat.service";
 
 @WebSocketGateway({ namespace: '/chat' })
 export class ChatGateway {
     @WebSocketServer()
     server: Server;
+
+    constructor(private readonly chatService: ChatService) {}
 
     async handleConnection(client: Socket) {}
 }
