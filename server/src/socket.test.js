6b32de4346e0bb7dedaa01f29a8e0f5e922395de
kamilawad1 <kamilawad15@gmail.com>
2024-05-12 16:44:51 +0300

    send the user ID as part of the handshake data

--- server/src/socket.test.js
+++ server/src/socket.test.js
@@ -3,4 +3,8 @@ const io = require('socket.io-client');
 const SERVER_URL = process.env.SOCKET_URL;
 const USER_ID = process.env.USER1_id;
 
-const socket = io(SERVER_URL, {});
+const socket = io(SERVER_URL, {
+    auth: {
+        userId: USER_ID,
+    },
+});
