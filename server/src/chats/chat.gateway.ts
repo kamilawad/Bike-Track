0ce5f9cd4e2aea9e280d4b32bb2d018ffa9f6781
kamilawad1 <kamilawad15@gmail.com>
2024-05-12 15:00:30 +0300

    backend - setup handle connection function

--- server/src/chats/chat.gateway.ts
+++ server/src/chats/chat.gateway.ts
@@ -1,9 +1,11 @@
 import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server } from 'socket.io';
+import { Socket } from "socket.io-client";
 
 @WebSocketGateway({ namespace: '/chat' })
 export class ChatGateway {
     @WebSocketServer()
     server: Server;
 
+    async handleConnection(client: Socket) {}
 }
