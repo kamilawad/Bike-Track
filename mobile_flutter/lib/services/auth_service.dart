b8b7492243ef735a8235e5c588b49d387e2b4c02
kamilawad1 <kamilawad15@gmail.com>
2024-05-13 02:04:04 +0300

    frontend - add signup service

--- mobile_flutter/lib/services/auth_service.dart
+++ mobile_flutter/lib/services/auth_service.dart
@@ -1,4 +1,22 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../models/user_model.dart';
 
 class AuthService {
   static const String baseUrl = 'http://192.168.0.105:3000/auth';
+
+  Future<UserModel> signUp(String fullName, String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/signup'),
+      body: {'fullName': fullName, 'email': email, 'password': password},
+    );
+
+    if (response.statusCode == 201) {
+      final userData = jsonDecode(response.body)['user'];
+      return UserModel.fromJson(userData);
+    } else {
+      throw Exception('Signup failed');
+    }
+  }
 }
