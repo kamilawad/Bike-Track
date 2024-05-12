a0a883af9d86ff59a99a673ed7d6bef8436305cf
kamilawad1 <kamilawad15@gmail.com>
2024-05-12 14:52:57 +0300

    backend - inject socket.IO server instance into the server property

--- server/src/chats/chat.gateway.ts
+++ server/src/chats/chat.gateway.ts
@@ -1,5 +1,9 @@
-import { WebSocketGateway } from "@nestjs/websockets";
-
+import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server } from 'socket.io';
 
 @WebSocketGateway({ namespace: '/chat' })
-export class ChatGateway {}
+export class ChatGateway {
+    @WebSocketServer()
+    server: Server;
+
+}
