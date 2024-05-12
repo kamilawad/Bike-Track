d22674d0767e93dbc90385e3dd0a6dd393c8d115
kamilawad1 <kamilawad15@gmail.com>
2024-05-12 13:59:12 +0300

    backend - remove events module from app module imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,7 +4,6 @@ import { UserModule } from './users/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { RoutePlanModule } from './routeplans/routeplan.module';
-import { EventsModule } from './gateway/Events.module';
 import { ChatModule } from './chats/chat.module';
 import { GroupChatModule } from './groupchats/group-chat.module';
 
@@ -18,7 +17,6 @@ import { GroupChatModule } from './groupchats/group-chat.module';
   UserModule,
   RoutePlanModule,
   AuthModule,
-  EventsModule,
   ChatModule,
   GroupChatModule
   ],
