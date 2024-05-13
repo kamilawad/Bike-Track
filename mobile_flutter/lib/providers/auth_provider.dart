5b3a955d23cc2512feb0fef1ab87436858c884e1
kamilawad1 <kamilawad15@gmail.com>
2024-05-13 22:29:30 +0300

    frontend - add token to auth provider

--- mobile_flutter/lib/providers/auth_provider.dart
+++ mobile_flutter/lib/providers/auth_provider.dart
@@ -3,11 +3,14 @@ import 'package:mobile_flutter/models/user_model.dart';
 
 class AuthProvider extends ChangeNotifier {
   UserModel? _user;
+  String? _token;
 
   UserModel? get user => _user;
+  String? get token => _token;
 
   void setUser(UserModel? user) {
     _user = user;
+    _token = token;
     notifyListeners();
   }
 }
