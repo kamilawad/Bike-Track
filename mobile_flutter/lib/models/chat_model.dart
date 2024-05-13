ea65d762961bff08c76433f69251159a7117d547
kamilawad1 <kamilawad15@gmail.com>
2024-05-14 02:46:12 +0300

    frontend - create chat class

--- mobile_flutter/lib/models/chat_model.dart
+++ mobile_flutter/lib/models/chat_model.dart
@@ -1,6 +1,18 @@
-
-
 import 'package:mobile_flutter/models/user_model.dart';
+
+class Chat {
+  final String id;
+  final UserModel user1;
+  final UserModel user2;
+  final List<Message> messages;
+
+  Chat({
+    required this.id,
+    required this.user1,
+    required this.user2,
+    required this.messages,
+  });
+}
 
 class Message {
   final String id;
