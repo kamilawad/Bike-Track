259b0abb098f08ac446db41eb5e059c4a0b80cdf
kamilawad1 <kamilawad15@gmail.com>
2024-05-14 02:38:26 +0300

    frontend - add chats endpoint

--- mobile_flutter/lib/utils/constants.dart
+++ mobile_flutter/lib/utils/constants.dart
@@ -1,6 +1,7 @@
 class Constants {
   static const String baseUrl = 'http://192.168.0.105:3000';
   static const String authUrl = '$baseUrl/auth';
+  static const String chatUrl = '$baseUrl/chats';
 
   static const String chatSocketUrl = '$baseUrl/chat';
 }
