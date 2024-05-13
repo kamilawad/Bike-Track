fbc64131ed520eddefac092dbdd43d133d2fc7d6
kamilawad1 <kamilawad15@gmail.com>
2024-05-13 22:27:16 +0300

    frontend - add get and set user methods and notify listeners

--- mobile_flutter/lib/providers/auth_provider.dart
+++ mobile_flutter/lib/providers/auth_provider.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_flutter/models/user_model.dart';
 
 class AuthProvider extends ChangeNotifier {
-  
+  UserModel? _user;
+
+  UserModel? get user => _user;
+
+  void setUser(UserModel? user) {
+    _user = user;
+    notifyListeners();
+  }
 }
