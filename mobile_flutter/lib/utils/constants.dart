25b1317d1be035f10fc24661861bcad9915c239c
kamilawad1 <kamilawad15@gmail.com>
2024-05-13 21:49:56 +0300

    frontend - create file for contants

---
+++ mobile_flutter/lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const String baseUrl = 'http://192.168.0.105:3000';
+  static const String authUrl = '$baseUrl/auth';
+
+  static const String chatSocketUrl = '$baseUrl/chat';
+}
