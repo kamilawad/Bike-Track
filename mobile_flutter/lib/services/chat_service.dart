88206e94bc0c980efbd90903d44bb30cf0b8f414
kamilawad1 <kamilawad15@gmail.com>
2024-05-14 03:10:05 +0300

    frontend - create get chat service

--- mobile_flutter/lib/services/chat_service.dart
+++ mobile_flutter/lib/services/chat_service.dart
@@ -1,5 +1,25 @@
+import 'dart:convert';
+
+import 'package:mobile_flutter/models/chat_model.dart';
+import 'package:mobile_flutter/providers/auth_provider.dart';
 import 'package:mobile_flutter/utils/constants.dart';
+import 'package:http/http.dart' as http;
 
 class ChatService {
   final String chatUrl = Constants.baseUrl;
+
+  Future<List<Chat>> fetchChats(AuthProvider authProvider) async {
+    final token = authProvider.token;
+    final url = Uri.parse('$chatUrl/chats');
+    final headers = {'Authorization': 'Bearer $token'};
+
+    final response = await http.get(url, headers: headers);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonChats = jsonDecode(response.body);
+      return jsonChats.map((json) => Chat.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to fetch chats');
+    }
+  }
 }
