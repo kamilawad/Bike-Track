14e6c7adc9853db7e44dac65e62f66d16740a944
kamilawad1 <kamilawad15@gmail.com>
2024-05-14 12:46:53 +0300

    backend - setup event module

---
+++ server/src/events/event.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { AuthModule } from "src/auth/auth.module";
+
+@Module({
+    imports: [
+        AuthModule,
+    ],
+    controllers: [],
+    providers: []
+})
+export class EventModule {}
