d852287da2624bc774db1a0b321228250cf0f852
kamilawad1 <kamilawad15@gmail.com>
2024-05-14 13:21:35 +0300

    backend - create join event service

--- server/src/events/event.service.ts
+++ server/src/events/event.service.ts
@@ -4,12 +4,34 @@ import { Model } from "mongoose";
 import { GroupChat } from "src/schemas/groupchat.schema";
 import { User } from "src/schemas/user.schema";
 import { Event } from "src/schemas/event.schema";
+import { GroupChatService } from "src/groupchats/group-chat.service";
 
 @Injectable()
 export class EventService {
     constructor(
         @InjectModel(Event.name) private eventModel: Model<Event>,
         @InjectModel(User.name) private userModel: Model<User>,
-        @InjectModel(GroupChat.name) private groupChatModel: Model<GroupChat>
+        @InjectModel(GroupChat.name) private groupChatModel: Model<GroupChat>,
+        private groupChatService: GroupChatService,
     ) {}
+
+    async joinEvent(eventId: string, userId: string): Promise<Event> {
+        const event = await this.eventModel.findById(eventId);
+        const user = await this.userModel.findById(userId);
+      
+        if (!event || !user) {
+          throw new NotFoundException('Event or user not found');
+        }
+      
+        if (event.members.some((participant) => participant._id.toString() === user._id.toString())) {
+          throw new BadRequestException('User is already a participant of this event');
+        }
+      
+        event.members.push(user);
+        const savedEvent = await event.save();
+      
+        await this.groupChatService.joinGroupChat(savedEvent.groupChat._id, userId);
+      
+        return savedEvent;
+    }
 }
