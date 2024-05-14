fa3bf46f59e9ea4d0e0ef54d0401878b289b8306
kamilawad1 <kamilawad15@gmail.com>
2024-05-14 10:40:06 +0300

    backend - create socket server instance

--- server/src/groupchats/group-chat.gateway.ts
+++ server/src/groupchats/group-chat.gateway.ts
@@ -1,4 +1,7 @@
-import { WebSocketGateway } from "@nestjs/websockets";
+import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server } from 'socket.io';
 
 @WebSocketGateway({ namespace: '/group-chat' })
-export class GroupChatGateway {}
+export class GroupChatGateway {
+    @WebSocketServer() server: Server;
+}
