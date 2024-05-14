dcafe2be60fd0c35ddde7d53d0a68f549e0754ff
kamilawad1 <kamilawad15@gmail.com>
2024-05-14 03:12:31 +0300

    frontend - fetch chat messages service

--- mobile_flutter/lib/services/chat_service.dart
+++ mobile_flutter/lib/services/chat_service.dart
@@ -22,4 +22,19 @@ class ChatService {
       throw Exception('Failed to fetch chats');
     }
   }
+
+  Future<List<Message>> fetchMessages(String chatId, AuthProvider authProvider) async {
+    final token = authProvider.token;
+    final url = Uri.parse('$chatUrl/chats/$chatId');
+    final headers = {'Authorization': 'Bearer $token'};
+
+    final response = await http.get(url, headers: headers);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonMessages = jsonDecode(response.body)['messages'];
+      return jsonMessages.map((json) => Message.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to fetch messages');
+    }
+  }
 }
