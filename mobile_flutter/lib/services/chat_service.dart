1bf2cfbcd212a6d821cea54b2bd5877450d92671
kamilawad1 <kamilawad15@gmail.com>
2024-05-14 03:15:08 +0300

    frontend - calling send message api

--- mobile_flutter/lib/services/chat_service.dart
+++ mobile_flutter/lib/services/chat_service.dart
@@ -10,7 +10,7 @@ class ChatService {
 
   Future<List<Chat>> fetchChats(AuthProvider authProvider) async {
     final token = authProvider.token;
-    final url = Uri.parse('$chatUrl/chats');
+    final url = Uri.parse(chatUrl);
     final headers = {'Authorization': 'Bearer $token'};
 
     final response = await http.get(url, headers: headers);
@@ -25,7 +25,7 @@ class ChatService {
 
   Future<List<Message>> fetchMessages(String chatId, AuthProvider authProvider) async {
     final token = authProvider.token;
-    final url = Uri.parse('$chatUrl/chats/$chatId');
+    final url = Uri.parse('$chatUrl/$chatId');
     final headers = {'Authorization': 'Bearer $token'};
 
     final response = await http.get(url, headers: headers);
@@ -35,6 +35,21 @@ class ChatService {
       return jsonMessages.map((json) => Message.fromJson(json)).toList();
     } else {
       throw Exception('Failed to fetch messages');
+    }
+  }
+
+  Future<Message> sendMessage(String chatId, String content, AuthProvider authProvider) async {
+    final token = authProvider.token;
+    final url = Uri.parse('$chatUrl/$chatId/send-message');
+    final headers = {'Authorization': 'Bearer $token', 'Content-Type': 'application/json'};
+    final body = jsonEncode({'content': content});
+
+    final response = await http.post(url, headers: headers, body: body);
+
+    if (response.statusCode == 201) {
+      return Message.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to send message');
     }
   }
 }
