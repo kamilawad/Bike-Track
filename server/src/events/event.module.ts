2867c399593f8bad97013d6762fcc4ff4a10f475
kamilawad1 <kamilawad15@gmail.com>
2024-05-14 12:53:20 +0300

    backend - add controllers and providers

--- server/src/events/event.module.ts
+++ server/src/events/event.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "src/auth/auth.module";
+import { EventController } from "./event.controller";
+import { EventService } from "./event.service";
 
 @Module({
     imports: [
         AuthModule,
     ],
-    controllers: [],
-    providers: []
+    controllers: [EventController],
+    providers: [EventService]
 })
 export class EventModule {}
