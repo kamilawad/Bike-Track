459bea4c188dc0bcfd9b4b7191979e55da75d9aa
kamilawad1 <kamilawad15@gmail.com>
2024-05-14 13:34:49 +0300

    backend - add create event endpoint

--- server/src/events/event.controller.ts
+++ server/src/events/event.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Param, UseGuards, Request, Post } from "@nestjs/common";
+import { Controller, Param, UseGuards, Request, Post, Body } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { EventService } from "./event.service";
+import { CreateEventDto } from "./dto/create-event.dto";
 
 @Controller('events')
 @UseGuards(AuthGuard('jwt'))
 export class EventController {
     constructor(private readonly eventService: EventService) {}
+
+    @Post()
+    create(@Body() createEventDto: CreateEventDto, @Request() req) {
+        const userId = req.user._id;
+        return this.eventService.createEvent(createEventDto, userId);
+    }
 
     @Post(':id/join')
     joinEvent(@Param('id') id: string, @Request() req) {
