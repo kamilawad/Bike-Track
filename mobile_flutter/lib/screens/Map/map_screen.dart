77689a8e7b6ab5cd9bfa8cedf736290f0e647340
kamilawad1 <kamilawad15@gmail.com>
2024-05-15 15:16:57 +0300

    frontend - add google maps package

--- mobile_flutter/lib/screens/Map/map_screen.dart
+++ mobile_flutter/lib/screens/Map/map_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
