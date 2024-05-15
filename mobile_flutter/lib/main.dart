0fb9f8de42c2364ceb45bef997507afd3c8cfa15
kamilawad1 <kamilawad15@gmail.com>
2024-05-15 16:03:22 +0300

    frontend - setup google map configuration

--- mobile_flutter/lib/main.dart
+++ mobile_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mobile_flutter/providers/auth_provider.dart';
 import 'package:mobile_flutter/providers/chat_provider.dart';
 import 'package:mobile_flutter/screens/Auth/login_screen.dart';
@@ -14,7 +15,8 @@ import 'package:mobile_flutter/screens/Onboarding/welcome_screen.dart';
 import 'package:provider/provider.dart';
 import 'utils/theme_util.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(
     MultiProvider(
       providers: [
