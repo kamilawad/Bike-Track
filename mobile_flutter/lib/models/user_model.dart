d5b4b7bd6c9cf6ed433b46e9b8f5b0e952366eb0
kamilawad1 <kamilawad15@gmail.com>
2024-05-16 02:15:54 +0300

    frontend - update user model

--- mobile_flutter/lib/models/user_model.dart
+++ mobile_flutter/lib/models/user_model.dart
@@ -1,7 +1,7 @@
 class UserModel {
   final String id;
   final String fullName;
-  final String email;
+  final String? email;
 
   UserModel( { required this.id, required this.fullName, required this.email} );
 
