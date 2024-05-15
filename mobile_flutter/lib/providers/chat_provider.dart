77fed458de66027c15c239840dd9f009233a0038
kamilawad1 <kamilawad15@gmail.com>
2024-05-16 02:16:54 +0300

    update chat provider

--- mobile_flutter/lib/providers/chat_provider.dart
+++ mobile_flutter/lib/providers/chat_provider.dart
@@ -1,25 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_flutter/models/chat_model.dart';
-import 'package:mobile_flutter/providers/auth_provider.dart';
-import 'package:mobile_flutter/services/chat_service.dart';
-import 'package:mobile_flutter/services/chat_socket.dart';
-import 'package:provider/provider.dart';
+//import 'package:mobile_flutter/models/chat_model.dart';
+//import 'package:mobile_flutter/services/chat_service.dart';
+//import 'package:mobile_flutter/services/chat_socket.dart';
 
 class ChatProvider extends ChangeNotifier {
-  final ChatService _chatService = ChatService();
-  final WebSocketService _webSocketService = WebSocketService();
+  /*final ChatService _chatService;
+  final WebSocketService _webSocketService;
+
   List<Chat> _chats = [];
   Chat? _currentChat;
   List<Message> _messages = [];
-  List<Map<String, dynamic>> _userChatsWithLastMessage = [];
 
-  List<Map<String, dynamic>> get userChatsWithLastMessage => _userChatsWithLastMessage;
+  ChatProvider(this._chatService, this._webSocketService);
+
   List<Chat> get chats => _chats;
   Chat? get currentChat => _currentChat;
   List<Message> get messages => _messages;
 
-  void connectToWebSocket(BuildContext context) {
-    _webSocketService.connect(context);
+  Future<void> fetchChats(String token) async {
+    try {
+      _chats = await _chatService.fetchChats(token);
+      notifyListeners();
+    } catch (e) {
+      print('Failed to fetch chats: $e');
+    }
+  }
+
+  Future<void> fetchMessages(String chatId, String token) async {
+    try {
+      _messages = await _chatService.fetchMessages(chatId, token);
+      _currentChat = _chats.firstWhere((chat) => chat.id == chatId);
+      notifyListeners();
+    } catch (e) {
+      print('Failed to fetch messages: $e');
+    }
+  }
+
+  Future<void> sendMessage(String chatId, String content, String token) async {
+    try {
+      await _chatService.sendMessage(chatId, content, token);
+      _webSocketService.sendMessage(chatId, content);
+      notifyListeners();
+    } catch (e) {
+      print('Failed to send message: $e');
+    }
+  }
+
+  void connectToWebSocket(String userId) {
+    _webSocketService.connect(c);
     _webSocketService.registerNewMessageListener(_handleNewMessage);
   }
 
@@ -28,46 +56,9 @@ class ChatProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> fetchChats(BuildContext context) async {
-    try {
-      final authProvider = Provider.of<AuthProvider>(context, listen: false);
-      print('authhhhh nnn:${authProvider.token}');
-      final chats = await _chatService.fetchChats(authProvider.token!);
-      print(chats);
-      _chats = chats;
-      notifyListeners();
-    } catch (e) {
-      print('Failed to fetch chats: $e');
-    }
-  }
-
-  Future<void> fetchMessages(String chatId, BuildContext context) async {
-    try {
-      final authProvider = Provider.of<AuthProvider>(context, listen: false);
-      final messages = await _chatService.fetchMessages(chatId, authProvider.token!);
-      _messages = messages;
-      _currentChat = _chats.firstWhere((chat) => chat.id == chatId);
-      notifyListeners();
-    } catch (e) {
-      print('Failed to fetch messages: $e');
-    }
-  }
-
-  Future<void> sendMessage(String chatId, String content, BuildContext context) async {
-    try {
-      final authProvider = Provider.of<AuthProvider>(context, listen: false);
-      final message = await _chatService.sendMessage(chatId, content, authProvider.token!);
-      _messages.add(message);
-      _webSocketService.sendMessage(chatId, content);
-      notifyListeners();
-    } catch (e) {
-      print('Failed to send message: $e');
-    }
-  }
-
   @override
   void dispose() {
     _webSocketService.disconnect();
     super.dispose();
-  }
+  }*/
 }
