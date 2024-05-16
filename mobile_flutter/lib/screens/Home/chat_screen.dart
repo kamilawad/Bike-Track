03825945d2c4423cdffcb655792af346cced5a58
kamilawad1 <kamilawad15@gmail.com>
2024-05-16 16:35:09 +0300

    frontend - display message date

--- mobile_flutter/lib/screens/Home/chat_screen.dart
+++ mobile_flutter/lib/screens/Home/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:mobile_flutter/models/chat_model.dart';
 import 'package:mobile_flutter/providers/auth_provider.dart';
 import 'package:mobile_flutter/screens/Home/chat_details_screen.dart';
@@ -21,14 +22,14 @@ class _ChatScreenState extends State<ChatScreen> {
   late io.Socket socket;
 
   @override
-void initState() {
-  super.initState();
-  _chatService = ChatService();
-  _authProvider = Provider.of<AuthProvider>(context, listen: false);
-  _webSocketService = WebSocketService(context);
-  _webSocketService.connect(context);
-  _fetchChats();
-}
+  void initState() {
+    super.initState();
+    _chatService = ChatService();
+    _authProvider = Provider.of<AuthProvider>(context, listen: false);
+    _webSocketService = WebSocketService(context);
+    _webSocketService.connect(context);
+    _fetchChats();
+  }
 
   Future<void> _fetchChats() async {
     setState(() {});
@@ -62,27 +63,42 @@ void initState() {
                 final otherUser = chat.user1.id == _authProvider.user!.id
                     ? chat.user2
                     : chat.user1;
-                  return ListTile(
-                    leading: CircleAvatar(
-                      child: Text(otherUser.fullName[0]),
-                    ),
-                    title: Text(otherUser.fullName),
-                    subtitle: Text(
-                      chat.messages.isNotEmpty
-                          ? chat.messages.last.content
-                          : 'No messages',
-                    ),
-                    trailing: chat.messages.isNotEmpty
-                        ? Text(chat.messages.last.sentAt.toString())
-                        : null,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ChatDetailScreen(chat: chat),
-                        ),
-                      );
-                    },
+
+                final sentAt = DateTime.parse(chat.messages.last.sentAt.toString());
+                final now = DateTime.now();
+                final difference = now.difference(sentAt);
+
+                String formattedDate;
+                if (difference.inDays == 0) {
+                  formattedDate = 'Today, ${DateFormat('hh:mm a').format(sentAt)}';
+                } else if (difference.inDays == 1) {
+                  formattedDate = 'Yesterday, ${DateFormat('hh:mm a').format(sentAt)}';
+                } else if (difference.inDays < 7) {
+                  formattedDate = '${DateFormat('EEEE').format(sentAt)}, ${DateFormat('hh:mm a').format(sentAt)}';
+                } else {
+                  formattedDate = DateFormat('M/d/yyyy').format(sentAt);
+                }
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Text(otherUser.fullName[0]),
+                  ),
+                  title: Text(otherUser.fullName),
+                  subtitle: Text(
+                    chat.messages.isNotEmpty
+                        ? chat.messages.last.content
+                        : 'No messages',
+                  ),
+                  trailing: chat.messages.isNotEmpty
+                      ? Text(formattedDate)
+                      : null,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatDetailScreen(chat: chat),
+                      ),
+                    );
+                  },
                 );
               },
             );
