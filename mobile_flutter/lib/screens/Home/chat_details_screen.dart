f5157e34259973e380bcdfbc36b90a4a65e7c9d0
kamilawad1 <kamilawad15@gmail.com>
2024-05-16 11:06:20 +0300

    frontend - add messages screen

--- mobile_flutter/lib/screens/Home/chat_details_screen.dart
+++ mobile_flutter/lib/screens/Home/chat_details_screen.dart
@@ -1,3 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_flutter/models/chat_model.dart';
+import 'package:mobile_flutter/providers/auth_provider.dart';
+import 'package:mobile_flutter/services/chat_socket.dart';
+import 'package:provider/provider.dart';
+
+class ChatDetailScreen extends StatefulWidget {
+  final Chat chat;
+
+  const ChatDetailScreen({super.key, required this.chat});
+
+  @override
+  _ChatDetailScreenState createState() => _ChatDetailScreenState();
+}
+
+class _ChatDetailScreenState extends State<ChatDetailScreen> {
+  late WebSocketService _webSocketService;
+  late AuthProvider _authProvider;
+  final TextEditingController _messageController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _authProvider = Provider.of<AuthProvider>(context, listen: false);
+    _webSocketService = WebSocketService(context);
+    _webSocketService.registerNewMessageListener(_handleNewMessage);
+  }
+
+  void _handleNewMessage(Message message) {
+    setState(() {
+      // Update the chat messages with the new message
+    });
+  }
+
+  void _sendMessage() {
+    final message = _messageController.text;
+    if (message.isNotEmpty) {
+      _webSocketService.sendMessage(widget.chat.id, message);
+      _messageController.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final otherUser = getOtherUser(widget.chat, _authProvider.user!.id);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(otherUser.fullName),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.chat.messages.length,
+              itemBuilder: (context, index) {
+                final message = widget.chat.messages[index];
+                return ListTile(
+                  title: Text(message.content),
+                  // Add additional UI elements for message metadata, etc.
+                );
+              },
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _messageController,
+                  decoration: const InputDecoration(
+                    hintText: 'Type a message...',
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: _sendMessage,
+                icon: const Icon(Icons.send),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+}
+
+
 /*import 'package:flutter/material.dart';
 import 'package:mobile_flutter/models/chat_model.dart';
 import 'package:mobile_flutter/providers/chat_provider.dart';
