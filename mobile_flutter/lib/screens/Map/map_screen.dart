110a3ad2821b22dccabe06aab3c33e735384b81e
kamilawad1 <kamilawad15@gmail.com>
2024-05-16 17:43:56 +0300

    frontend - add polyline package

--- mobile_flutter/lib/screens/Map/map_screen.dart
+++ mobile_flutter/lib/screens/Map/map_screen.dart
@@ -1,9 +1,8 @@
 import 'dart:async';
-
 import 'package:location/location.dart';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
