b9060c87bc8da8cbbdf26220abfde11bb2884e57
kamilawad1 <kamilawad15@gmail.com>
2024-05-16 11:05:48 +0300

    frontend - update chat screen

--- mobile_flutter/lib/screens/Home/chat_screen.dart
+++ mobile_flutter/lib/screens/Home/chat_screen.dart
@@ -1,4 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_flutter/models/chat_model.dart';
+import 'package:mobile_flutter/providers/auth_provider.dart';
+import 'package:mobile_flutter/screens/Home/chat_details_screen.dart';
+import 'package:mobile_flutter/services/chat_service.dart';
+import 'package:mobile_flutter/services/chat_socket.dart';
+import 'package:provider/provider.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  late ChatService _chatService;
+  late AuthProvider _authProvider;
+  late WebSocketService _webSocketService;
+  late io.Socket socket;
+
+  @override
+void initState() {
+  super.initState();
+  _chatService = ChatService();
+  _authProvider = Provider.of<AuthProvider>(context, listen: false);
+  _webSocketService = WebSocketService(context);
+  _webSocketService.connect(context);
+  _fetchChats();
+}
+
+  Future<void> _fetchChats() async {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _webSocketService.disconnect();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chats'),
+      ),
+      body: FutureBuilder<List<Chat>>(
+        future: _chatService.fetchChats(_authProvider.token!),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final chats = snapshot.data!;
+            return ListView.builder(
+              itemCount: chats.length,
+              itemBuilder: (context, index) {
+                final chat = chats[index];
+                final otherUser = chat.user1.id == _authProvider.user!.id
+                    ? chat.user2
+                    : chat.user1;
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Text(otherUser.fullName[0]),
+                    ),
+                    title: Text(otherUser.fullName),
+                    subtitle: Text(
+                      chat.messages.isNotEmpty
+                          ? chat.messages.last.content
+                          : 'No messages',
+                    ),
+                    trailing: chat.messages.isNotEmpty
+                        ? Text(chat.messages.last.sentAt.toString())
+                        : null,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChatDetailScreen(chat: chat),
+                        ),
+                      );
+                    },
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+
+/*import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class ChatPage extends StatefulWidget {
@@ -95,7 +191,7 @@ class _ChatPageState extends State<ChatPage> {
       ),
     );
   }
-}
+}*/
 
 
 /*import 'package:flutter/material.dart';
