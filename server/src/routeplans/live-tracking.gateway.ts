d315b1eaa676f041fb270c5f1be00efdbf16af2b
kamilawad1 <kamilawad15@gmail.com>
2024-05-16 15:36:03 +0300

    backend - add start tracking event

--- server/src/routeplans/live-tracking.gateway.ts
+++ server/src/routeplans/live-tracking.gateway.ts
@@ -10,7 +10,14 @@ import { Server, Socket } from 'socket.io';
 export class LiveTrackingGateway {
     @WebSocketServer() server: Server;
     private connectedUsers = new Map<string, Socket>();
-    
+
+    @SubscribeMessage('startTracking')
+    handleStartTracking(@ConnectedSocket() client: Socket) {
+        const userId = this.getUserIdFromClient(client);
+        this.connectedUsers.set(userId, client);
+        console.log(`User ${userId} started tracking`);
+    }
+
     private getUserIdFromClient(client: Socket): string {
         return client.handshake.auth.userId;
     }
