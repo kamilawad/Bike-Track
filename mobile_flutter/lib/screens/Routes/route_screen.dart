206d74528d6a8cb8fec293f5a4a9a6e8db1dc0e3
kamilawad1 <kamilawad15@gmail.com>
2024-05-19 04:30:48 +0300

    frontend - update route screen

--- mobile_flutter/lib/screens/Routes/route_screen.dart
+++ mobile_flutter/lib/screens/Routes/route_screen.dart
@@ -1,206 +1,180 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:mobile_flutter/screens/Routes/location_service.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
 
 class RouteScreen extends StatefulWidget {
-  const RouteScreen({super.key});
-
   @override
-  State<RouteScreen> createState() => _RouteScreenState();
+  _RouteScreenState createState() => _RouteScreenState();
 }
 
 class _RouteScreenState extends State<RouteScreen> {
-  Completer<GoogleMapController> _controller = Completer();
-  TextEditingController _originController = TextEditingController();
-  TextEditingController _destinationController = TextEditingController();
-  Set<Marker> _markers = Set<Marker>();
-  Set<Polyline> _polylines = Set<Polyline>();
-  List<LatLng> polylinePoints = <LatLng>[];
-  int _polylineIdCounter = 1;
-
-  static final CameraPosition _initialCameraPosition = CameraPosition(
-    target: LatLng(33.8547, 35.8623),
-    zoom: 7.0,
-  );
+  final Completer<GoogleMapController> _mapController = Completer();
+  Map<PolylineId, Polyline> polylines = {};
+  List<LatLng> polylinePoints = [];
+  Map<MarkerId, Marker> markers = {};
+  LatLng? startPoint;
 
   @override
   void initState() {
     super.initState();
   }
 
-  void _setMarker(LatLng point) {
-    setState(() {
-      _markers.add(
-        Marker(
-          markerId: MarkerId('marker'),
-          position: point,
-        ),
+  Future<List<LatLng>> getPolylinePoints(List<LatLng> points) async {
+    List<LatLng> polylineCoordinates = [];
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    for (int i = 0; i < points.length - 1; i++) {
+      PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        "AIzaSyB23mdww8kGOa4BnrL3higCEWp0H-KzkQo",
+        PointLatLng(points[i].latitude, points[i].longitude),
+        PointLatLng(points[i + 1].latitude, points[i + 1].longitude),
+        travelMode: TravelMode.driving,
       );
+
+      if (result.points.isNotEmpty) {
+        result.points.forEach((PointLatLng point) {
+          polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+        });
+      } else {
+        print(result.errorMessage);
+      }
+    }
+
+    return polylineCoordinates;
+  }
+
+  void generatePolyLineFromPoints(List<LatLng> polylineCoordinates) async {
+    PolylineId id = PolylineId("poly");
+    Polyline polyline = Polyline(
+      polylineId: id,
+      color: Colors.red,
+      points: polylineCoordinates,
+      width: 4,
+    );
+
+    setState(() {
+      polylines[id] = polyline;
+      _addMarkers(polylineCoordinates.first, polylineCoordinates.last);
+    });
+
+    LatLngBounds bounds = _calculateBounds(polylineCoordinates);
+    final GoogleMapController controller = await _mapController.future;
+    controller.animateCamera(CameraUpdate.newLatLngBounds(bounds, 50));
+  }
+
+  LatLngBounds _calculateBounds(List<LatLng> coordinates) {
+    double? minLat, minLong, maxLat, maxLong;
+    for (LatLng coord in coordinates) {
+      minLat = (minLat == null || coord.latitude < minLat)
+          ? coord.latitude
+          : minLat;
+      minLong = (minLong == null || coord.longitude < minLong)
+          ? coord.longitude
+          : minLong;
+      maxLat = (maxLat == null || coord.latitude > maxLat)
+          ? coord.latitude
+          : maxLat;
+      maxLong = (maxLong == null || coord.longitude > maxLong)
+          ? coord.longitude
+          : maxLong;
+    }
+    return LatLngBounds(
+      southwest: LatLng(minLat!, minLong!),
+      northeast: LatLng(maxLat!, maxLong!),
+    );
+  }
+
+  void resetRoute() {
+    setState(() {
+      polylines.clear();
+      polylinePoints.clear();
+      markers.clear();
+      startPoint = null;
     });
   }
 
-  void _setPolyline(List<LatLng> points) {
-    final String polylineIdVal = 'polyline_$_polylineIdCounter';
-    _polylineIdCounter++;
-    _polylines.add(
-      Polyline(
-        polylineId: PolylineId(polylineIdVal),
-        width: 3,
-        color: Colors.blue,
-        points: points,
+  void _addMarkers(LatLng start, LatLng end) {
+    markers.clear();
+
+    final MarkerId startMarkerId = MarkerId('start');
+    final Marker startMarker = Marker(
+      markerId: startMarkerId,
+      position: start,
+      infoWindow: InfoWindow(title: 'Start'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(
+        BitmapDescriptor.hueGreen,
       ),
     );
+
+    final MarkerId endMarkerId = MarkerId('end');
+    final Marker endMarker = Marker(
+      markerId: endMarkerId,
+      position: end,
+      infoWindow: InfoWindow(title: 'End'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(
+        BitmapDescriptor.hueRed,
+      ),
+    );
+
+    markers[startMarkerId] = startMarker;
+    markers[endMarkerId] = endMarker;
+    startPoint = start;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Routes'),
+        title: Text('Route Map'),
         elevation: 0,
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    height: 48.0,
-                    child: TextField(
-                      controller: _originController,
-                      decoration: InputDecoration(
-                        hintText: 'Origin',
-                        prefixIcon: Icon(Icons.location_on),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        contentPadding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10.0),
-                Expanded(
-                  child: SizedBox(
-                    height: 48.0,
-                    child: TextField(
-                      controller: _destinationController,
-                      decoration: InputDecoration(
-                        hintText: 'Destination',
-                        prefixIcon: Icon(Icons.location_on),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        contentPadding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10.0),
-                Material(
-                  elevation: 5.0,
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: InkWell(
-                    onTap: () async {
-                      var directions = await LocationService().getDirections(
-                        _originController.text,
-                        _destinationController.text,
-                        );
-                        if (directions.isNotEmpty) {
-                          _goToPlace(directions);
-                          _setPolyline(directions['polyline_decoded']);
-                        } else {
-                          print('No directions found');
-                        }
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10.0),
-                        border: Border.all(
-                          color: Color(0xFFF05206),
-                          width: 2.0,
-                        ),
-                      ),
-                      child: const Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.search,
-                            color: Color(0xFFF05206),
-                          ),
-                          SizedBox(width: 5.0),
-                          Text(
-                            'Search',
-                            style: TextStyle(
-                              color: Color(0xFFF05206),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: GoogleMap(
-              mapType: MapType.normal,
-              markers: _markers,
-              polylines: _polylines,
-              initialCameraPosition: _initialCameraPosition,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-              onTap: (point) {
-                setState(() {
-                  polylinePoints.add(point);
-                  _setPolyline(polylinePoints);
-                });
-              },
-            ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: resetRoute,
           ),
         ],
+      ),
+      body: GoogleMap(
+        onMapCreated: (GoogleMapController controller) {
+          _mapController.complete(controller);
+        },
+        initialCameraPosition: CameraPosition(
+          target: LatLng(33.8938, 35.5018),
+          zoom: 12.0,
+        ),
+        polylines: Set<Polyline>.of(polylines.values),
+        markers: Set<Marker>.of(markers.values),
+        onTap: (LatLng point) async {
+          if (startPoint != null && _isNearStartPoint(point, startPoint!, 70)) {
+            setState(() {
+              polylinePoints.add(startPoint!);
+            });
+            List<LatLng> coordinates = await getPolylinePoints(polylinePoints);
+            generatePolyLineFromPoints(coordinates);
+          } else {
+            setState(() {
+              polylinePoints.add(point);
+            });
+
+            if (polylinePoints.length > 1) {
+              List<LatLng> coordinates = await getPolylinePoints(polylinePoints);
+              generatePolyLineFromPoints(coordinates);
+            }
+          }
+        },
       ),
     );
   }
 
-  Future<void> _goToPlace(Map<String, dynamic> directions) async {
-    final GoogleMapController controller = await _controller.future;
-
-    if (directions.containsKey('start_location') && directions['start_location'] != null) {
-      final double lat = directions['start_location']['lat'];
-      final double lng = directions['start_location']['lng'];
-
-      controller.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(target: LatLng(lat, lng), zoom: 12),
-        ),
-      );
-
-      _setMarker(LatLng(lat, lng));
-    }
-
-    if (directions.containsKey('bounds_ne') && directions.containsKey('bounds_sw') && directions['bounds_ne'] != null && directions['bounds_sw'] != null) {
-      final Map<String, dynamic> boundsNe = directions['bounds_ne'];
-      final Map<String, dynamic> boundsSw = directions['bounds_sw'];
-
-      controller.animateCamera(
-        CameraUpdate.newLatLngBounds(
-          LatLngBounds(
-            southwest: LatLng(boundsSw['lat'], boundsSw['lng']),
-            northeast: LatLng(boundsNe['lat'], boundsNe['lng']),
-          ),
-          25,
-        ),
-      );
-    }
+  bool _isNearStartPoint(LatLng point, LatLng startPoint, double radius) {
+    double distance = Geolocator.distanceBetween(
+      point.latitude,
+      point.longitude,
+      startPoint.latitude,
+      startPoint.longitude,
+    );
+    return distance <= radius;
   }
 }
