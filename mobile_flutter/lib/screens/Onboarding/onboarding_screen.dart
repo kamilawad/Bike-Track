34cb9fa2a23a091df3d4f9cd7043b83434b674cb
kamilawad1 <kamilawad15@gmail.com>
2024-05-19 13:45:16 +0300

    update onboarding screen

--- mobile_flutter/lib/screens/Onboarding/onboarding_screen.dart
+++ mobile_flutter/lib/screens/Onboarding/onboarding_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:mobile_flutter/data/static/onboardingdata.dart';
 import 'package:mobile_flutter/widgets/Onboarding/onboarding_widget.dart';
@@ -11,8 +13,37 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-  final PageController _controller = PageController();
-  
+  final PageController _controller = PageController(initialPage: 0);
+  int _currentPage = 0;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 4), (timer) {
+      if (_currentPage == onboarding.length - 1) {
+        _controller.jumpToPage(0);
+        _currentPage = 0;
+      } else {
+        _controller.nextPage(
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+        );
+        _currentPage++;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,13 +52,17 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           children: [
             PageView(
               controller: _controller,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentPage = index;
+                });
+              },
               children: [
                 OnboardingWidget(model: onboarding[0]),
                 OnboardingWidget(model: onboarding[1]),
                 OnboardingWidget(model: onboarding[2]),
               ],
             ),
-            
             Positioned(
               bottom: 180,
               left: 0,
@@ -35,12 +70,20 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               child: Center(
                 child: SmoothPageIndicator(
                   controller: _controller,
-                  count: 3,
+                  count: onboarding.length,
                   effect: const ScrollingDotsEffect(
                     dotWidth: 9,
                     dotHeight: 9,
                     activeDotColor: Color(0xFFF05206),
                   ),
+                  onDotClicked: (index) {
+                    _controller.animateToPage(
+                      index,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    );
+                    _currentPage = index;
+                  },
                 ),
               ),
             ),
